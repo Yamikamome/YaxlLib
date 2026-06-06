e8051efd06bb8ef8c8e8d37ea94569fcd15c602a
Yamikamome <Yamikamome2@gmail.com>
2026-06-07 06:11:14 +0900

    add: インクルードをまとめたヘッダーを追加

---
+++ YaxlLibSolution/YaxlLib/YaxlLib.h
@@ -0,0 +1,22 @@
+﻿// -----------------------------------------------------------------------------------------
+//  Author        : Yamikamome
+//  Description	  : YaxlLibの全機能を一括インクルードできるヘッダー
+// -----------------------------------------------------------------------------------------
+
+#ifndef YAXL_LIB_H_
+#define YAXL_LIB_H_
+
+#include "YaxlLibLink.h"
+
+/*
+*	コア
+*/
+#include "Core/Input/YaxlInput.h"
+#include "Core/Time/YaxlTime.h"
+
+/*
+*	グラフィック
+*/
+#include "Graphics/YaxlGraphicsDevice.h"
+
+#endif
