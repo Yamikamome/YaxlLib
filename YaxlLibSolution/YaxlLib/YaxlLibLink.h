97f0251dd20810e81af0a14728fdd924ac068e81
Yamikamome <Yamikamome2@gmail.com>
2026-06-07 06:10:05 +0900

    add: プロパティ指定を省略する指定を追加

---
+++ YaxlLibSolution/YaxlLib/YaxlLibLink.h
@@ -0,0 +1,14 @@
+﻿// -----------------------------------------------------------------------------------------
+//  Author        : Yamikamome
+//  Description	  : 外部インクルードのディレクトリ設定を、
+//					ユーザー側が行わなくていいようにする定義
+// -----------------------------------------------------------------------------------------
+
+#ifndef YAXL_LIB_LINK_H_
+#define YAXL_LIB_LINK_H_
+
+#pragma comment(lib, "opengl32.lib")
+#pragma comment(lib, "glfw3.lib")
+#pragma comment(lib, "YaxlLib.lib")
+
+#endif
