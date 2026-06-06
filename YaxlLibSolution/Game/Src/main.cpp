87c439789511c3590e5aed60496e4ec9c3ef5b18
Yamikamome <Yamikamome2@gmail.com>
2026-06-07 06:11:55 +0900

    add: ライブラリの実装を確認するためのゲームクラスを追加

---
+++ YaxlLibSolution/Game/Src/main.cpp
@@ -0,0 +1,46 @@
+﻿#include "YaxlLib/YaxlGame.h"
+#include <stdio.h>
+
+class MyGame : public Yaxl::Game {
+public:
+	MyGame() : Yaxl::Game(1280, 720, "YaxlLib", 60, 20) {}
+
+protected:
+	void start() override {
+
+	}
+
+	void tick() override {
+
+	}
+
+	void update() override {
+
+	}
+
+	void draw() const override {
+
+	}
+
+	void end() override {
+
+	}
+
+	bool is_running() const override {
+		// パッドのBackとStartが両方押されていたら終了する
+		if (Yaxl::Input::IsPadButton(Yaxl::PadButton::Back) && Yaxl::Input::IsPadButton(Yaxl::PadButton::Start)) {
+			return false;
+		}
+		// キーボードのEscapeが押されていたら終了する
+		if (Yaxl::Input::IsKey(Yaxl::KeyCode::Escape)) {
+			return false;
+		}
+
+		// 上記に該当しなければ、ゲーム続行
+		return true;
+	}
+};
+
+int main() {
+    return MyGame().run();
+}
