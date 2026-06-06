d2365894e942621d18376d40d11be71c26791862
Yamikamome <Yamikamome2@gmail.com>
2026-06-07 06:10:48 +0900

    add: 時間管理を追加

---
+++ YaxlLibSolution/YaxlLib/Core/Time/YaxlTime.cpp
@@ -0,0 +1,45 @@
+#include "YaxlTime.h"
+#include "YaxlTimeInternal.h"
+
+using namespace Yaxl;
+
+// FPS制限値
+static int target_fps_ = 0;
+// 経過時間（秒）
+static float delta_time_ = 0.0f;
+// 1秒間にTickを実行する目標回数
+static int tick_rate_ = 20;
+// Tickの実行回数
+static unsigned long long tick_count_ = 0;
+
+void Yaxl::SetTargetFps(int fps) {
+	target_fps_ = fps;
+}
+
+int Yaxl::GetTargetFps() {
+	return target_fps_;
+}
+
+int Yaxl::GetTickRate() {
+	return tick_rate_;
+}
+
+float Yaxl::GetDeltaTime() {
+	return delta_time_;
+}
+
+unsigned long long Yaxl::GetTickCount() {
+	return tick_count_;
+}
+
+void Yaxl::Internal::SetDeltaTime(float delta_time) {
+	delta_time_ = delta_time;
+}
+
+void Yaxl::Internal::SetTickRate(int tick_rate) {
+	tick_rate_ = tick_rate;
+}
+
+void Yaxl::Internal::IncrementTickCount() {
+	tick_count_++;
+}
---
+++ YaxlLibSolution/YaxlLib/Core/Time/YaxlTime.h
@@ -0,0 +1,42 @@
+// -----------------------------------------------------------------------------------------
+//  Author        : Yamikamome
+//  Description	  : 時間を管理する
+// -----------------------------------------------------------------------------------------
+
+#ifndef YAXL_TIME_H_
+#define YAXL_TIME_H_
+
+namespace Yaxl {
+    /// <summary>
+    /// FPSの制限値を設定（0を指定すると可変FPSになる）
+    /// </summary>
+    /// <param name="fps">= FPS</param>
+    void SetTargetFps(int fps = 0);
+
+    /// <summary>
+    /// 現在設定されているFPSの制限値を取得
+    /// </summary>
+    /// <returns></returns>
+    int GetTargetFps();
+
+    /// <summary>
+    /// 1秒間にTickを実行する目標回数を取得
+    /// </summary>
+    /// <returns>目標回数</returns>
+    int GetTickRate();
+
+    /// <summary>
+    /// 前回のフレームからかかった時間（秒）を取得
+    /// </summary>
+    /// <returns>経過時間（秒）</returns>
+    float GetDeltaTime();
+
+    /// <summary>
+    /// ゲーム開始からTickが実行された累計回数を取得
+    /// </summary>
+    /// <returns>累計回数</returns>
+    unsigned long long GetTickCount();
+
+}	// namespace Yaxl end
+
+#endif
