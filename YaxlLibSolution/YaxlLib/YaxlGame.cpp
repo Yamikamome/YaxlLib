8d4edca59689d94722f2b85f5a61eb765bfd8a34
Yamikamome <Yamikamome2@gmail.com>
2026-06-07 06:11:39 +0900

    add: ゲームクラスを追加

---
+++ YaxlLibSolution/YaxlLib/YaxlGame.cpp
@@ -0,0 +1,112 @@
+﻿#include "YaxlGame.h"
+
+#include <GLFW/glfw3.h>
+#include "Core/Time/YaxlTimeInternal.h"
+
+using namespace Yaxl;
+
+Yaxl::Game::Game(int width, int height, const char* title, int target_fps, int tick_rate) {
+    // グラフィックデバイスの生成と初期化
+    graphics_device_ = new GraphicsDevice();
+    bool is_success = graphics_device_->Init(width, height, title);
+
+	// 失敗したらグラフィックデバイスを解放
+    if (is_success == false) {
+		ClearGraphicsDevice();
+		return;
+    }
+
+	// 初期設定の保存
+	Yaxl::SetTargetFps(target_fps);
+	Yaxl::Internal::SetTickRate(tick_rate);
+}
+
+Yaxl::Game::~Game() {
+	ClearGraphicsDevice();
+}
+
+int Yaxl::Game::run() {
+	if (graphics_device_ == nullptr) {
+		return -1;
+	}
+
+	// 実行中にする
+	is_running_ = true;
+
+	// 開始処理を呼び出す
+	start();
+
+	// 時間管理の初期化
+	double prev_time = glfwGetTime();
+	double tick_accumulator = 0.0f;
+
+	// 1tickあたりに必要な時間（秒）を計算する
+	const double tick_interval = 1.0f / Yaxl::GetTickRate();
+
+	// メインループ
+	while (is_running_ == true) {
+		// ウィンドウを閉じる必要があれば抜ける
+		if (graphics_device_->IsWindowClosed()) {
+			break;
+		}
+		// ゲームの実行が終了したら抜ける
+		if (is_running() == false) {
+			break;
+		}
+
+		// 経過時間を取得
+		const double current_time = glfwGetTime();
+		const double frame_time = current_time - prev_time;
+
+		// FPSを制限する
+		const int target_fps = Yaxl::GetTargetFps();
+		if (target_fps > 0) {
+			const double target_frame_time = 1.0 / target_fps;
+			// 目標時間に達していない
+			if (frame_time < target_frame_time) {
+				continue;
+			}
+		}
+
+		// 入力の更新
+		Yaxl::Input::Update(graphics_device_);
+
+		// 時間の更新
+		prev_time = current_time;
+		Yaxl::Internal::SetDeltaTime(static_cast<float>(frame_time));
+
+		// tick処理を呼び出す
+		tick_accumulator += frame_time;
+		while (tick_accumulator >= tick_interval) {
+			tick();
+			Yaxl::Internal::IncrementTickCount();
+			tick_accumulator -= tick_interval;
+		}
+
+
+		// 更新処理を呼び出す
+		update();
+		// 描画処理を呼び出す
+		draw();
+
+		// ウィンドウに描画結果を表示
+		graphics_device_->SwapBuffers();
+	}
+
+	// 終了処理を呼び出す
+	end();
+
+	// 実行終了
+	is_running_ = false;
+	// グラフィックデバイスを解放
+	ClearGraphicsDevice();
+
+	return 0;
+}
+
+void Yaxl::Game::ClearGraphicsDevice() {
+	if (graphics_device_ != nullptr) {
+		delete graphics_device_;
+		graphics_device_ = nullptr;
+	}
+}
---
+++ YaxlLibSolution/YaxlLib/YaxlGame.h
@@ -0,0 +1,86 @@
+﻿// -----------------------------------------------------------------------------------------
+//  Author        : Yamikamome
+//  Description	  : YaxlLibを使ったゲーム制作を行うためのクラス
+// -----------------------------------------------------------------------------------------
+
+#ifndef YAXL_GAME_H_
+#define YAXL_GAME_H_
+
+#include "YaxlLib.h"
+
+class GraphicsDevice;
+
+namespace Yaxl {
+class Game {
+public:
+    /// <summary>
+    /// コンストラクタ
+    /// </summary>
+    /// <param name="width">= ウィンドウの幅</param>
+    /// <param name="height">= ウィンドウの高さ</param>
+    /// <param name="title">= ウィンドウのタイトル</param>
+    /// <param name="target_fps">= FPSの制限値</param>
+    /// <param name="tick_rate">= 1秒間にTickを実行する目標回数</param>
+    Game(int width = 640, int height = 480, const char* title = "YaxlGame", int target_fps = 0, int tick_rate = 20);
+
+    virtual ~Game();
+
+public:
+    /// <summary>
+    /// 実行
+    /// </summary>
+    /// <returns>状態を返す</returns>
+    int run();
+
+public:
+    /// <summary>
+    /// ゲームの開始
+    /// </summary>
+    virtual void start() {};
+
+    /// <summary>
+    /// Tickの更新
+    /// </summary>
+    virtual void tick() {};
+
+    /// <summary>
+    /// ゲームの更新
+    /// </summary>
+    virtual void update() {};
+
+    /// <summary>
+    /// ゲームの描画
+    /// </summary>
+    virtual void draw() const {};
+
+    /// <summary>
+    /// ゲームの終了
+    /// </summary>
+    virtual void end() {};
+
+    /// <summary>
+    /// ゲームが実行中かどうか
+    /// </summary>
+    /// <returns>実行中なら真を、終了するなら偽を返す</returns>
+    virtual bool is_running() const { return true; }
+
+private:
+	/// <summary>
+	/// 管理中のグラフィックデバイスを解放
+	/// </summary>
+	void ClearGraphicsDevice();
+
+private:
+    // グラフィックデバイス
+    GraphicsDevice* graphics_device_{ nullptr };
+    // ゲームループが実行中かどうか
+    bool is_running_{ false };
+
+public:
+    // コピー禁止
+    Game(const Game& other) = delete;
+    Game& operator = (const Game& other) = delete;
+};
+}	// namespace Yaxl end
+
+#endif
