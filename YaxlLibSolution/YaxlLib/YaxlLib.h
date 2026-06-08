48d411841e866be135fed88d4ca4d8ad77d2f682
Yamikamome <Yamikamome2@gmail.com>
2026-06-08 14:42:11 +0900

    YaxlLibに組み込み

--- YaxlLibSolution/YaxlLib/YaxlLib.h
+++ YaxlLibSolution/YaxlLib/YaxlLib.h
@@ -1,4 +1,4 @@
-﻿// -----------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------------------
 //  Author        : Yamikamome
 //  Description	  : YaxlLibの全機能を一括インクルードできるヘッダー
 // -----------------------------------------------------------------------------------------
@@ -18,5 +18,18 @@
 *	グラフィック
 */
 #include "Graphics/YaxlGraphicsDevice.h"
+
+/*
+*	数学系
+*/
+#include "Math/YaxlColor.h"
+#include "Math/YaxlMath.h"
+#include "Math/YaxlMatrix3x3.h"
+#include "Math/YaxlMatrix4x4.h"
+#include "Math/YaxlQuaternion.h"
+#include "Math/YaxlRandom.h"
+#include "Math/YaxlVector2.h"
+#include "Math/YaxlVector3.h"
+#include "Math/YaxlVector4.h"
 
 #endif
