afb59188031e55f858d11c178e8356197067b245
Yamikamome <Yamikamome2@gmail.com>
2026-06-18 15:58:31 +0900

    debug releaseでlibを切り替え

--- YaxlLibSolution/YaxlLib/YaxlLibLink.h
+++ YaxlLibSolution/YaxlLib/YaxlLibLink.h
@@ -6,6 +6,16 @@
 #ifndef YAXL_LIB_LINK_H_
 #define YAXL_LIB_LINK_H_
 
+#ifdef _MSC_VER // Visual Studioでコンパイルされている場合のみ有効
+#ifdef _DEBUG
+	// デバッグモードの時は _d 付きのライブラリを探してリンクする
+#pragma comment(lib, "YaxlLib_d.lib")
+#else
+	// リリースモードの時は通常のライブラリを探してリンクする
+#pragma comment(lib, "YaxlLib.lib")
+#endif
+#endif
+
 #pragma comment(lib, "opengl32.lib")
 #pragma comment(lib, "glfw3.lib")
 #pragma comment(lib, "YaxlLib.lib")
